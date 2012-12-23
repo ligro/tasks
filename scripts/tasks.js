c520ea97295aa1cdeddaf0e7091a9c3596f93cb0
Vincent Daligault <vincent.daligault@laposte.net>
2012-12-23 23:30:49 +0100

    add skeleton of js Application

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,15 +1,22 @@
 (function( $ ) {
     $.App = {
         init: function(el) {
-            console.log(el)
-
+            $('a.jLink').bind('click', function(){
+                var func = $(this).data('func');
+                if (typeof $.App[func] == 'function') {
+                    $.App[func].call();
+                }
+            });
             // retrieve tasks lists
+        },
+        addtask: function(){
+            $('<div></div>').dialog();
+            // display form (dialog)
+            // submit form and close dialog
         }
     };
 })( jQuery );
 
-$(document).ready(function()
-    {
-        $.App.init(document.getElementsByTagName('body'));
-    }
-);
+$(document).ready(function() {
+    $.App.init(document.getElementsByTagName('body'));
+});
