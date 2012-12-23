758b0aed04c354306e0d5f307e4992a6994a268e
Vincent Daligault <vincent.daligault@laposte.net>
2012-12-23 18:30:17 +0100

    init js application

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,4 +1,15 @@
-// javascript file
+(function( $ ) {
+    $.App = {
+        init: function(el) {
+            console.log(el)
 
+            // retrieve tasks lists
+        }
+    };
+})( jQuery );
 
-
+$(document).ready(function()
+    {
+        $.App.init(document.getElementsByTagName('body'));
+    }
+);
