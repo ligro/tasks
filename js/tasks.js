abc6570c4108dafd826082a928b05780972fc71d
Vincent Daligault <vincent.daligault@laposte.net>
2013-02-07 01:37:18 +0100

    add and use modal plugin

--- js/tasks.js
+++ js/tasks.js
@@ -1,7 +1,7 @@
-(function($) {
+;(function($) {
     'use strict';
 
-    var App = {
+     $.App = {
 
         templates: {},
         tasks: {},
@@ -9,8 +9,8 @@
         init: function(el) {
             $('a.jLink').bind('click', function(){
                 var func = $(this).data('func');
-                if (typeof App[func] == 'function') {
-                    App[func].call();
+                if (typeof $.App[func] == 'function') {
+                    $.App[func].call();
                 }
             });
 
@@ -23,7 +23,7 @@
                 dataType: 'json',
                 timeout: 300,
                 success: function(data){
-                    App.templates = data;
+                    $.App.templates = data;
                 },
                 error: function(xhr, type){
                     // @TODO show ui error to specify we can't run
@@ -38,7 +38,7 @@
                 dataType: 'json',
                 timeout: 300,
                 success: function(data){
-                    App.tasks = data;
+                    $.App.tasks = data;
                     // trigger event to display data
                 },
                 error: function(xhr, type){
@@ -47,37 +47,30 @@
             });
 
         },
-        _loadTpl: function(name) {
+        _loadTpl: function(name, data, end) {
             typeof dust.cache[name] === 'undefined'
-                && dust.loadSource(dust.compile(App.templates[name], name));
+                && dust.loadSource(dust.compile($.App.templates[name], name))
+
+            dust.render(name, data, end)
         },
         abouts: function(){
-            App._loadTpl("abouts");
-            dust.render("abouts", {}, function(err, out) {
-                // @TODO put it somewhere else
-                $(".container .modal").html(out);
-                // @TODO allow to close the new window
-            });
+            $('<div>')
+                .modal('abouts', {}, {title: "Abouts"})
         },
         addtask: function(){
-            App._loadTpl("addTask");
-
-            // will be usefull for i18n
-            dust.render("addTask", {}, function(err, out) {
-                // display the view en the end of the page
-                console.log($(".container .modal"));
-                $(".container .modal").html(out);
-
-                // @TODO allow to close the new window
-            });
-
-            // submit form and close dialog
+            $('<div>')
+                .modal('addTask', {}, {
+                    title: "Add task",
+                    buttons: [
+                        {name: 'Close'},
+                        {name: 'Add', class: "primary-btn"}
+                    ]
+                })
         }
     };
 
     Zepto(function($){
-        App.init(document.getElementsByTagName('body'));
+        $.App.init(document.getElementsByTagName('body'));
     })
 
-})(Zepto);
-
+})(Zepto)
