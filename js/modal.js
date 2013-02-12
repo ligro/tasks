3598facbefb90c95e7555c308c62d022b4cd80cf
Vincent Daligault <vincent.daligault@laposte.net>
2013-02-13 00:28:26 +0100

    modal enhancement

--- js/modal.js
+++ js/modal.js
@@ -2,20 +2,22 @@
     'use strict';
 
     $.extend($.fn, {
-        modal: function(tpl, data, modalData)
-        {
+        modal: function(tpl, data, modalData){
             var $this = $(this)
 
             $.App._loadTpl(tpl, data, function(err, out) {
+
                 modalData.content = out
                 $.App._loadTpl('modal', modalData, function(err, out) {
+
                     $this.html($(out))
+                    // add close event
                     $this.on('click', '.close', function(){ $this.remove() })
                     $('body').append($this)
-
-                    // add events on buttons
                 })
             })
+
+            return $this
         }
     })
 })(Zepto)
