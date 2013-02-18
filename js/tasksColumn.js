035c3556bff937ff9d0f982683622892d5db60e7
Vincent Daligault <vincent.daligault@laposte.net>
2013-02-18 23:49:46 +0100

    display task in backlog

--- js/tasksColumn.js
+++ js/tasksColumn.js
@@ -7,10 +7,6 @@
                 var $this = $(this),
                    status = $this.data('status')
 
-                if (typeof status === 'undefined') {
-                    return
-                }
-
                 $.task.findByStatus($this.data('status')).forEach(function(task){
                     $.App._loadTpl('taskColumn', task, function(err, out) {
                         $this.find('.tasks').append($(out))
