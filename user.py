70fd7d4b260a29ba2f698d7cee86ed93b46125f6
Vincent Daligault <vincent.daligault@laposte.net>
2013-05-19 16:55:23 +0200

    begin /user/create/

--- user.py
+++ user.py
@@ -1,14 +1,39 @@
 import cherrypy
+from model import Model
+
+import pprint
 
 class Controller:
     """User controller"""
 
     @cherrypy.expose
-    def create(self):
-        # TODO
-        pass
+    @cherrypy.tools.json_out()
+    def create(self, **kw):
+        formFields = [
+                {'name': 'pseudo'},
+                {'name': 'email'},
+                {'name': 'password'},
+                {'name': 'password_conf'}
+        ]
+        errors = {}
+        for field in formFields:
+            pprint.pprint(field)
+            if field['name'] not in kw:
+                # TODO return error msg
+                return False
 
-class User:
+        datas = kw
+        objId = User.save(datas)
+        user = ts.collection.find_one(objId)
+        return user
+
+class User(Model):
     """user object"""
-    pass
+    def __init__(self):
+        super(User, self).__init__()
+        self.collection = self.db.user
 
+    def _validate(self, datas):
+        datas = super(User, self)._validate(datas)
+        # todo
+        return datas
