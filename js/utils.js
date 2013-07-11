348457460a2eb7e3664f82b8582b729e6459b4b0
Vincent Daligault <vincent.daligault@laposte.net>
2013-07-11 23:42:17 +0200

    rewrite zepto plugin, and add some fixes

--- js/utils.js
+++ js/utils.js
@@ -17,38 +17,47 @@
         },
         // post form
         post: function(success){
-            var $this = this
-            $.ajax({
-                type: 'POST',
-                url: this.data('url'),
-                data: this.getFields(),
-                success: function(data){
-                    if (data.success) {
-                        $.App[$this.data('success')]()
 
-                    } else if (typeof data.msgs === "undefined") {
-                        $(document.body).trigger('notify', ['An error occured', 'error']);
-                    } else {
-                        var $input, field;
-                        for (field in data.msgs) {
-                            $input = $this.find('input[name="'+field+'"]')
-                            if ($input.length == 0) {
-                                $(document.body).trigger('notify', [data.msgs[field], 'error']);
-                                continue;
+            return this.each(function(){
+                var $this = $(this)
+
+                $this.find('.control-group').removeClass('error')
+                $this.find('.jErrorMsg').remove()
+
+                $.ajax({
+                    type: $this.attr('method'),
+                    url: $this.data('url'),
+                    data: $this.getFields(),
+                    success: function(data){
+                        if (data.success) {
+                            $.App[$this.data('success')]()
+
+                        } else if (typeof data.msgs === "undefined") {
+                            $(document.body).trigger('notify', ['An error occured', 'error']);
+                        } else {
+                            var $input, field;
+                            for (field in data.msgs) {
+                                $input = $this.find('input[name="'+field+'"]')
+                                if ($input.length == 0) {
+                                    $(document.body).trigger('notify', [data.msgs[field], 'error']);
+                                    continue;
+                                }
+                                $input.errorMsg(data.msgs[field])
                             }
-                            $input.errorMsg(data.msgs[field])
                         }
+                    },
+                    error: function(xhr, type){
+                        $(document.body).trigger('notify', ['An error occured', 'error']);
                     }
-                },
-                error: function(xhr, type){
-                    $(document.body).trigger('notify', ['An error occured', 'error']);
-                }
+                })
             })
         },
         errorMsg: function(msg) {
-            $(this)
-                .after('<span class="help-inline">'+msg+'</span>')
-                .closest('.control-group').addClass('error')
+            return this.each(function(){
+                $(this)
+                    .after('<span class="jErrorMsg help-inline">'+msg+'</span>')
+                    .closest('.control-group').addClass('error')
+            })
         }
     })
 })(Zepto)
