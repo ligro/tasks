a173a3ee38d5a1c8a87e3fd58f64c9d675a08c77
Vincent Daligault <vincent.daligault@laposte.net>
2014-01-20 23:17:20 +0100

    fix issue with search

--- search.py
+++ search.py
@@ -23,15 +23,10 @@ def flush():
 def query(query, limit, offset=0):
     _index._sconn.reopen()
     q = _index._sconn.query_parse(query, default_op=_index._sconn.OP_OR)
-    #if auth.userAuth is not None:
-    #    fq = _index._sconn.query_field('authorId', auth.userAuth['_id'])
-    #q = _index._sconn.query_filter(q, fq)
-    # --
-    import pprint
-
+    if auth.userAuth is not None:
+        fq = _index._sconn.query_field('authorId', auth.userAuth['_id'])
+    q = _index._sconn.query_filter(q, fq)
     rows = _index._sconn.search(q, offset, limit)
-    # --
-    pprint.pprint(len(rows))
 
     results = []
     for row in rows:
@@ -50,23 +45,27 @@ class Index:
 
     def __del__(self):
         if self._sconn:
+            self._sconn.close()
+        if self._iconn:
             self._iconn.flush()
             self._iconn.close()
-            self._sconn.close()
 
     def create_index(self, dbpath):
         """Create a new index, and set up its field structure.
         """
-        print "create_index"
         self._iconn = xappy.IndexerConnection(dbpath)
 
         self._iconn.add_field_action('id', xappy.FieldActions.INDEX_EXACT)
+        self._iconn.add_field_action('id', xappy.FieldActions.STORE_CONTENT)
         self._iconn.add_field_action('authorId', xappy.FieldActions.INDEX_EXACT)
 
         self._iconn.add_field_action('tag', xappy.FieldActions.INDEX_EXACT)
-        self._iconn.add_field_action('tag', xappy.FieldActions.FACET, type='string')
+        self._iconn.add_field_action('tag', xappy.FieldActions.STORE_CONTENT)
+        # FIXME fatal error
+        #self._iconn.add_field_action('tag', xappy.FieldActions.FACET, type='string')
 
         self._iconn.add_field_action('task', xappy.FieldActions.INDEX_FREETEXT, language='en')
+        self._iconn.add_field_action('task', xappy.FieldActions.STORE_CONTENT)
 
         self._sconn = xappy.SearchConnection(dbpath)
 
