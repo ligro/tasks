a92935b6a6a551c039bb7390d410e53d30000af6
Vincent Daligault <vincent.daligault@laposte.net>
2014-02-08 22:59:34 +0100

    Update indexing

--- search.py
+++ search.py
@@ -1,9 +1,12 @@
 # -*- encoding: UTF-8 -*-
 import os
 import xapian
+import json
 
 import auth
 from taskconfig import config
+
+lang = 'en'
 
 def index(task):
 
@@ -12,14 +15,24 @@ def index(task):
     doc.set_data(task['task'])
     indexer = _index.get_indexer()
     indexer.set_document(doc)
+
+    # index text
     indexer.index_text(task['task'])
-    
+
+    # index values
     doc.add_value(0, task['_id'])
     doc.add_value(1, task['authorId'])
     if 'tag' in task:
         doc.add_value(2, ' '.join(task['tag']))
 
-    _index.add_doc(doc)
+    # store data
+    doc.set_data(json.dumps(task))
+
+    # add id
+    idterm = u"Q" + task['_id']
+    doc.add_boolean_term(idterm)
+
+    _index.add_doc(idterm, doc)
 
 def flush():
     _index.flush()
@@ -29,6 +42,11 @@ def query(query, limit, offset=0):
     q = _index.query_parser.parse_query(query)
     enquire = _index.get_enquire()
     enquire.set_query(q)
+
+    # facetting
+    spy = xapian.ValueCountMatchSpy(1)
+    enquire.add_matchspy(spy)
+
     #if auth.userAuth is not None:
     #    fq = _index._sconn.query_field('authorId', auth.userAuth['_id'])
     #q = _index._sconn.query_filter(q, fq)
@@ -71,7 +89,7 @@ class Index:
     def flush(self):
         self._idb.flush()
 
-    def add_doc(self, doc):
-        self._idb.add_document(doc)
+    def add_doc(self, id, doc):
+        self._idb.replace_document(id, doc)
 
 _index = Index()
