5dae65854a3bb4b445a64f1614a78b451725ef05
Vincent Daligault <ligro@daligro.net>
2015-06-17 14:00:16 +0200

    beautifuler select for dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,72 +4,75 @@
     var dashboards = {
         $el: null,
         current: null,
-        add: function(id, name) {
-            dashboards.$el.append('<option value="' + id + '">' + name + '</option>')
-        },
         select: function(id) {
-            console.log("select %s", id);
             if (dashboards.current != id) {
                 dashboards.current = id
                 $(document.body).trigger('dashboard:change', [dashboards.current])
                 window.localStorage.setItem('dashboardId', dashboards.current)
             }
-        }
-    }
+        },
+        init: function() {
+            dashboards.$el = $('.jDashboard')
 
-    Zepto(function($){
-        dashboards.$el = $('.jDashboard')
+            $(document).one('templates:load', function(e){
+                $.ajax({
+                    type: 'GET',
+                    url: '/dashboards',
+                    success: function(data){
+                        if (data.length == 0) {
+                            $('#FatalError').show()
+                            return
+                        }
+                        var defaultDashboardId = false,
+                            options = {}
 
-        $(document).one('templates:load', function(e){
-            $.ajax({
-                type: 'GET',
-                url: '/dashboards',
-                success: function(data){
-                    if (data.length == 0) {
-                        $('#FatalError').show()
-                        return
-                    }
-                    var defaultDashboardId = false
+                        for (var dashboardId in data) {
+                            !defaultDashboardId && (defaultDashboardId = dashboardId)
+                            options[dashboardId] = data[dashboardId].name
+                        }
+                        options[""] = "All my tasks"
+                        options["add"] = "Add a new dashboard"
 
-                    for (var dashboardId in data) {
-                        !defaultDashboardId && (defaultDashboardId = dashboardId)
-                        dashboards.add(dashboardId, data[dashboardId].name)
-                    }
-                    dashboards.add("", "All my tasks")
-                    dashboards.add("add", "Add a new dashboard")
+                        dashboards.$el.select(options)
 
-                    dashboards.$el.on('change', function (e) {
-                        if (dashboards.$el.val() == 'add') {
-                            // launch modal to add a new dashboard
-                             $('<form class="jForm" action="/dashboard/add" method="POST">')
-                                .on('post:success', function (e, data){
-                                    // FIXME doesn't work
-                                    dashboards.$el.val(data.datas.id)
-                                    dashboards.select(data.datas.id)
-                                    $.modal.closeModal()
-                                })
+                        dashboards.$el.on('select:change', function (e, id) {
+                            console.log('select:change', id)
+                            if (id == 'add') {
+                                // launch modal to add a new dashboard
+                                $('<form class="jForm" action="/dashboard/add" method="POST">')
+                                    .on('post:success', function (e, data){
+                                        // FIXME doesn't work
+                                        dashboards.$el.val(data.datas.id)
+                                            dashboards.select(data.datas.id)
+                                            $.modal.closeModal()
+                                    })
                                 .modal('addDashboard', {}, {
                                     title: 'Dashboard',
                                     submit: {name: 'Add', class: 'btn-primary'}
                                 })
 
-                        } else {
-                            dashboards.select(dashboards.$el.val())
-                        }
-                    })
+                            } else {
+                                dashboards.select(id)
+                            }
+                        })
 
-                    var currentDashboardId = window.localStorage.getItem('dashboardId')
-                    if (!currentDashboardId) {
-                        currentDashboardId = defaultDashboardId
+                        var currentDashboardId = window.localStorage.getItem('dashboardId')
+                        if (!currentDashboardId) {
+                            currentDashboardId = defaultDashboardId
+                        }
+                        dashboards.$el.val(currentDashboardId)
+                        dashboards.select(currentDashboardId)
+                    },
+                    error: function(xhr, type){
+                        $('#FatalError').show()
                     }
-                    dashboards.$el.val(currentDashboardId)
-                    dashboards.select(currentDashboardId)
-                },
-                error: function(xhr, type){
-                    $('#FatalError').show()
-                }
+                })
             })
-        })
+        }
+    }
+
+    Zepto(function($){
+        dashboards.init()
     })
 
 })(Zepto)
---
+++ js/select.js
@@ -0,0 +1,36 @@
+;(function($) {
+    'use strict';
+
+    $.extend($.fn, {
+        select: function(options, selectedValue = null){
+            return this.each(function (){
+                var $this = $(this),
+                    $dropDown = $this.find('.jSelectDropDown'),
+                    $value = $this.find('.jSelectValue'),
+                    first = true
+
+                for (var value in options) {
+                    if (first) {
+                        $value.html(options[value]).data('value', value)
+                        first = false
+                    }
+                    $dropDown.append('<div data-value="' + value + '">' + options[value] + '</div>')
+                }
+
+                $this.on('click', '.jSelectValue', function (e) {
+                    e.preventDefault()
+                    $dropDown.toggle()
+                })
+                $this.on('click', '.jSelectDropDown div', function (e) {
+                    e.preventDefault()
+                    var $self = $(e.target)
+                    $value.html($self.html()).data('value', $self.data('value'))
+                    $dropDown.toggle()
+                    $this.trigger('select:change', [$self.data('value')])
+                })
+            })
+        }
+    })
+
+})(Zepto)
+
