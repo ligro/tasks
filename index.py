bed602a6f3df9af3f093657cf18ffd22190f4049
Vincent Daligault <ligro@daligro.net>
2015-11-11 21:44:28 +0100

    move all static content under the static directory

--- index.py
+++ index.py
@@ -154,25 +154,15 @@ class App:
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 conf = {
-    '/js': {
+    '/static': {
         'tools.staticdir.on': True,
-        'tools.staticdir.dir': os.path.join(current_dir, 'js'),
-        'tools.staticdir.content_types': {'js': 'application/javascript'}
-    },
-    '/css': {
-        'tools.staticdir.on': True,
-        'tools.staticdir.dir': os.path.join(current_dir, 'css'),
+        'tools.staticdir.dir': os.path.join(current_dir, 'static'),
         'tools.staticdir.content_types': {
+            'js': 'application/javascript',
             'css': 'text/css',
-        }
-    },
-    '/img': {
-        'tools.staticdir.on': True,
-        'tools.staticdir.dir': os.path.join(current_dir, 'img'),
-        'tools.staticdir.content_types': {
             'png': 'image/png',
         }
-    }
+    },
 }
 
 def application(environ, start_response):
